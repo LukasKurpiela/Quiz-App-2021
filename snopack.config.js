edc8ac0cf4335c037024adccd1002b51ce81464c
Lukas <lukas.kurpiela@gmail.com>
2021-04-20 19:50:41 +0200

    Restructure with BEM and new folder structure CSS.

---
+++ snopack.config.js
@@ -0,0 +1,9 @@
+// snowpack.config.js
+// Example: Using Snowpack's built-in bundling support
+module.exports = {
+    optimize: {
+      bundle: true,
+      minify: true,
+      target: 'es2018',
+    },
+  };
